c969979b6f930c275930f2cd98476f6ec928d0ef
Pontus Lundin <lundin.pontus@gmail.com>
2015-09-02 00:26:25 +0200

    allow origin * some error handling

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 var express = require("express");
 var request = require('request');
 
+function extractText(htmlString) {
+  var startIndex = htmlString.indexOf('<body>') + 6;
+  var endIndex = htmlString.indexOf('</body>');
+
+  return removeTags(htmlString.substring(startIndex, endIndex));
+}
+
+function removeTags(htmlString) {
+  return htmlString.replace(/<\/?[\w=\ "]+>/g, '');
+}
+
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
-app.get("/", function (req, res) {
-  var query = req.query;
+app.all("/", function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
 
-  request({ url: query.url, json: true }, function (error, response, body) {
+  if (!req.query.url) {
+    res.status(400).json({ error: 'Missing url parameter'});
+    return;
+  }
+
+  console.log('requesting %s', req.query.url);
+  request({ url: req.query.url, json: true }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       res.json(body);
     } else {
-      res.error(error);
+      res.status(response.statusCode).json({ error: error || extractText(body) });
     }
   });
 });
